434e8da17a1d983db1210b16f6f58e73d8c792e7
broken1456 <135306654+broken1456@users.noreply.github.com>
2023-07-25 08:20:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923132812141', ' 𝚳𝚫𝐒𝚯𝚯𝚳', true],
+  ['923132812141', ' 𝚳𝚫𝐒𝚯𝚯𝚳', true], 
   [''] 
 ] //Number of owners
 
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = ' 𝚳𝚫𝐒𝚯𝚯𝚳'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = ' 𝚳𝚫𝐒𝚯𝚯𝚳' 
+global.author = ' 𝚳𝚫𝐒𝚯𝚯𝚳' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
